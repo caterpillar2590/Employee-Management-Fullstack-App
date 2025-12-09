db2b773c4fd760a6f58ab9f6644b50e9da13d1e7
caterpillar2590 <i211117@nu.edu.pk>
2025-12-09 15:08:18 +0500

    Add EmployeeControllerTest.java file
    Created a new test file for EmployeeController.
    
    Signed-off-by: caterpillar2590 <i211117@nu.edu.pk>

---
+++ backend/src/test/java/com/example/employeemanagement/EmployeeControllerTest.java
@@ -0,0 +1,5 @@
+// Create this NEW file in the test directory
+package com.employee.management.controller;
+
+// [PASTE THE ENTIRE EmployeeControllerTest.java CODE HERE]
+// Make sure your Employee class has @Builder annotation
